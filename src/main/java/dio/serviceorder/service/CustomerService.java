130cd7ef549aed5c3f0c48de55f23cee9b86fc86
masachetti <50852444+masachetti@users.noreply.github.com>
2022-07-12 20:59:41 -0400

    Update CustomerService.java

--- src/main/java/dio/serviceorder/service/CustomerService.java
+++ src/main/java/dio/serviceorder/service/CustomerService.java
@@ -1,14 +1,14 @@
 package dio.serviceorder.service;
 
 import dio.serviceorder.dto.CustomerDTO;
-import dio.serviceorder.exception.CustomerAlreadyCreated;
+import dio.serviceorder.exception.CustomerAlreadyCreatedException;
+import dio.serviceorder.exception.CustomerNotFoundException;
 import dio.serviceorder.mapper.CustomerMapper;
 import dio.serviceorder.model.Customer;
 import dio.serviceorder.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -20,13 +20,12 @@ public class CustomerService {
 
     private final CustomerMapper customerMapper = CustomerMapper.INSTANCE;
 
-    public CustomerDTO createCustomer(CustomerDTO customerDTO) throws CustomerAlreadyCreated {
+    public CustomerDTO createCustomer(CustomerDTO customerDTO) throws CustomerAlreadyCreatedException {
         Customer customer = customerMapper.toModel(customerDTO);
-        if (customer.getId() == null || !checkIfCustomerIdAlreadyBeUsed(customer)){
-            Customer savedCustomer = customerRepository.save(customer);
-            return customerMapper.toDTO(savedCustomer);
-        }
-        throw new CustomerAlreadyCreated(customer.getId());
+        if (customerDTO.getId() != null && checkIfIdExists(customer.getId()))
+            throw new CustomerAlreadyCreatedException(customer.getId());
+        Customer savedCustomer = customerRepository.save(customer);
+        return customerMapper.toDTO(savedCustomer);
     }
 
     public List<CustomerDTO> listAll(){
@@ -36,8 +35,20 @@ public class CustomerService {
                 .collect(Collectors.toList());
     }
 
-    private boolean checkIfCustomerIdAlreadyBeUsed(Customer customer){
-        Optional<Customer> customerOptional = customerRepository.findById(customer.getId());
+    public CustomerDTO findById(Long id) throws CustomerNotFoundException {
+        Customer foundCustomer = customerRepository.findById(id)
+                .orElseThrow(() -> new CustomerNotFoundException(id));
+        return customerMapper.toDTO(foundCustomer);
+    }
+
+    public void deleteById(Long id) throws CustomerNotFoundException {
+        if (!checkIfIdExists(id))
+            throw new CustomerNotFoundException(id);
+        customerRepository.deleteById(id);
+    }
+
+    private boolean checkIfIdExists(Long id){
+        Optional<Customer> customerOptional = customerRepository.findById(id);
         return customerOptional.isPresent();
     }
 
