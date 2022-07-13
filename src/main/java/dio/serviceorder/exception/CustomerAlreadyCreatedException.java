90782e201a8bfc520eb53b8b6ccedbfbcd5c3947
masachetti <50852444+masachetti@users.noreply.github.com>
2022-07-12 20:37:00 -0400

    Refactor - Change customer already created exception class name

--- src/main/java/dio/serviceorder/exception/CustomerAlreadyCreatedException.java
+++ src/main/java/dio/serviceorder/exception/CustomerAlreadyCreatedException.java
@@ -4,8 +4,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.BAD_REQUEST)
-public class CustomerAlreadyCreated extends Exception{
-    public CustomerAlreadyCreated(Long id) {
+public class CustomerAlreadyCreatedException extends Exception{
+    public CustomerAlreadyCreatedException(Long id) {
         super(String.format("Customer with id %d is already created", id));
     }
 }
