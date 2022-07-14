6c62fbdcd1c68f83f3ade72307ea5adc0bf6838a
masachetti <50852444+masachetti@users.noreply.github.com>
2022-07-14 18:33:38 -0400

    Fix - The id generation strategy was incorrect

--- src/main/java/dio/serviceorder/model/Customer.java
+++ src/main/java/dio/serviceorder/model/Customer.java
@@ -14,7 +14,7 @@ import javax.persistence.*;
 @AllArgsConstructor
 public class Customer {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @Column(nullable = false)
--- src/main/java/dio/serviceorder/model/ServiceOrder.java
+++ src/main/java/dio/serviceorder/model/ServiceOrder.java
@@ -15,7 +15,7 @@ import javax.persistence.*;
 @AllArgsConstructor
 public class ServiceOrder {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @ManyToOne
