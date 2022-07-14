134b9608434a95faab64a4cfef6272c4d12eca72
masachetti <50852444+masachetti@users.noreply.github.com>
2022-07-14 10:16:19 -0400

    Feat - Developed methods that are missing in the service order service
    - delete by id
    - update
    - find by id
    - list all
    - list all by closed status
    - list all by customer
    - list all by service type

--- src/main/java/dio/serviceorder/service/ServiceOrderService.java
+++ src/main/java/dio/serviceorder/service/ServiceOrderService.java
@@ -1,8 +1,9 @@
 package dio.serviceorder.service;
 
 import dio.serviceorder.dto.ServiceOrderDTO;
-import dio.serviceorder.exception.CustomerAlreadyExistsException;
+import dio.serviceorder.enums.ServiceType;
 import dio.serviceorder.exception.ServiceOrderAlreadyExistsException;
+import dio.serviceorder.exception.ServiceOrderNotFoundException;
 import dio.serviceorder.mapper.ServiceOrderMapper;
 import dio.serviceorder.model.Customer;
 import dio.serviceorder.model.ServiceOrder;
@@ -10,7 +11,9 @@ import dio.serviceorder.repository.ServiceOrderRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class ServiceOrderService {
@@ -21,40 +24,61 @@ public class ServiceOrderService {
 
     public ServiceOrderDTO create(ServiceOrderDTO serviceOrderDTO) throws ServiceOrderAlreadyExistsException {
         ServiceOrder serviceOrder = serviceOrderMapper.toModel(serviceOrderDTO);
-        if (serviceOrderDTO.getId() != null && checkIfIdExists(serviceOrderDTO.getId()))
+        if (serviceOrderDTO.getId() != null && checkIfServiceOrderExists(serviceOrderDTO.getId()))
             throw new ServiceOrderAlreadyExistsException(serviceOrderDTO.getId());
         ServiceOrder savedServiceOrder = serviceOrderRepository.save(serviceOrder);
         return serviceOrderMapper.toDTO(savedServiceOrder);
     }
 
-    public void deleteById(Long id){
+    public void deleteById(Long id) throws ServiceOrderNotFoundException {
+        if (!checkIfServiceOrderExists(id))
+            throw new ServiceOrderNotFoundException(id);
+        serviceOrderRepository.deleteById(id);
     }
 
-    public ServiceOrderDTO update(Long id, ServiceOrderDTO serviceOrderDTO){
-        return serviceOrderDTO;
+    public ServiceOrderDTO update(ServiceOrderDTO serviceOrderDTO) throws ServiceOrderNotFoundException {
+        if (!checkIfServiceOrderExists(serviceOrderDTO.getId()))
+            throw new ServiceOrderNotFoundException(serviceOrderDTO.getId());
+        ServiceOrder serviceOrder = serviceOrderMapper.toModel(serviceOrderDTO);
+        ServiceOrder savedServiceOrder = serviceOrderRepository.save(serviceOrder);
+        return serviceOrderMapper.toDTO(savedServiceOrder);
     }
 
-//    public ServiceOrderDTO findById(Long id){
-//
-//    }
-//
-//    public List<ServiceOrderDTO> findAll(){
-//
-//    }
-//
-//    public List<ServiceOrderDTO> findByClosedStatus(boolean closedStatus){
-//
-//    }
-//
-//    public List<ServiceOrderDTO> findByCustomerId(Long customerId){
-//
-//    }
-//
-//    public List<ServiceOrderDTO> findByServiceType(ServiceType type){
-//
-//    }
+    public ServiceOrderDTO findById(Long id) throws ServiceOrderNotFoundException {
+        ServiceOrder foundServiceOrder = serviceOrderRepository.findById(id)
+                .orElseThrow(()-> new ServiceOrderNotFoundException(id));
+        return serviceOrderMapper.toDTO(foundServiceOrder);
+    }
 
-    private boolean checkIfIdExists(Long id){
+    public List<ServiceOrderDTO> listAll(){
+        return serviceOrderRepository.findAll()
+                .stream()
+                .map(serviceOrderMapper::toDTO)
+                .collect(Collectors.toList());
+    }
+
+    public List<ServiceOrderDTO> listAllWithClosedStatus(Boolean closedStatus){
+        return serviceOrderRepository.findByClosed(closedStatus)
+                .stream()
+                .map(serviceOrderMapper::toDTO)
+                .collect(Collectors.toList());
+    }
+
+    public List<ServiceOrderDTO> listAllOfCustomer(Customer customer){
+        return serviceOrderRepository.findByCustomer(customer)
+                .stream()
+                .map(serviceOrderMapper::toDTO)
+                .collect(Collectors.toList());
+    }
+
+    public List<ServiceOrderDTO> listAllWithServiceType(ServiceType type){
+        return serviceOrderRepository.findByServiceType(type)
+                .stream()
+                .map(serviceOrderMapper::toDTO)
+                .collect(Collectors.toList());
+    }
+
+    private boolean checkIfServiceOrderExists(Long id){
         Optional<ServiceOrder> serviceOrderOptional = serviceOrderRepository.findById(id);
         return serviceOrderOptional.isPresent();
     }
--- src/test/java/dio/serviceorder/service/ServiceOrderServiceTest.java
+++ src/test/java/dio/serviceorder/service/ServiceOrderServiceTest.java
@@ -1,21 +1,24 @@
 package dio.serviceorder.service;
 
+import dio.serviceorder.builder.CustomerDTOBuilder;
 import dio.serviceorder.builder.ServiceOrderDTOBuilder;
 import dio.serviceorder.dto.ServiceOrderDTO;
-import dio.serviceorder.exception.CustomerAlreadyExistsException;
+import dio.serviceorder.enums.ServiceType;
 import dio.serviceorder.exception.ServiceOrderAlreadyExistsException;
+import dio.serviceorder.exception.ServiceOrderNotFoundException;
+import dio.serviceorder.mapper.CustomerMapper;
 import dio.serviceorder.mapper.ServiceOrderMapper;
+import dio.serviceorder.model.Customer;
 import dio.serviceorder.model.ServiceOrder;
 import dio.serviceorder.repository.ServiceOrderRepository;
-import org.hamcrest.MatcherAssert;
-import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
+import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
 
 import static org.hamcrest.MatcherAssert.*;
@@ -95,4 +98,214 @@ public class ServiceOrderServiceTest {
         assertThat(savedServiceOrderDTO.getType(), is(equalTo(serviceOrderDTO.getType())));
         assertThat(savedServiceOrderDTO.getClosed(), is(equalTo(serviceOrderDTO.getClosed())));
     }
+
+    @Test
+    void whenListServiceOrdersIsCalledThenReturnAListOfServiceOrders() {
+        // given
+        ServiceOrderDTO serviceOrderDTO = ServiceOrderDTOBuilder.builder().build().toServiceOrderDTO();
+        ServiceOrder serviceOrder = serviceOrderMapper.toModel(serviceOrderDTO);
+
+        // when
+        when(serviceOrderRepository.findAll()).thenReturn(Collections.singletonList(serviceOrder));
+
+        // then
+        List<ServiceOrderDTO> foundListOfServiceOrdersDTO = serviceOrderService.listAll();
+
+        assertThat(foundListOfServiceOrdersDTO.get(0), is(equalTo(serviceOrderDTO)));
+        assertThat(foundListOfServiceOrdersDTO, is(not(empty())));
+    }
+
+    @Test
+    void whenListServiceOrderIsCalledThenReturnAnEmptyList() {
+        // when
+        when(serviceOrderRepository.findAll()).thenReturn(Collections.emptyList());
+
+        // then
+        List<ServiceOrderDTO> foundListOfServiceOrdersDTO = serviceOrderService.listAll();
+
+        assertThat(foundListOfServiceOrdersDTO, is(empty()));
+    }
+
+    @Test
+    void whenFindByIdIsCalledWithAValidIdThenReturnTheServiceOrder() throws ServiceOrderNotFoundException {
+        // given
+        ServiceOrderDTO serviceOrderDTO = ServiceOrderDTOBuilder.builder().build().toServiceOrderDTO();
+        ServiceOrder serviceOrder = serviceOrderMapper.toModel(serviceOrderDTO);
+
+        // when
+        when(serviceOrderRepository.findById(serviceOrderDTO.getId())).thenReturn(Optional.of(serviceOrder));
+
+        // then
+        ServiceOrderDTO foundServiceOrderDTO = serviceOrderService.findById(serviceOrderDTO.getId());
+
+        assertThat(foundServiceOrderDTO.getId(), is(equalTo(serviceOrderDTO.getId())));
+        assertThat(foundServiceOrderDTO, is(equalTo(serviceOrderDTO)));
+    }
+
+    @Test
+    void whenFindByIdIsCalledWithAnInvalidIdThenAnExceptionShouldBeThrown() {
+        // when
+        when(serviceOrderRepository.findById(1L)).thenReturn(Optional.empty());
+
+        // then
+        assertThrows(ServiceOrderNotFoundException.class, ()-> serviceOrderService.findById(1L));
+    }
+
+    @Test
+    void whenListByClosedStatusIsCalledThenAListOfServiceOrderShouldBeReturned() {
+        // given
+        ServiceOrderDTO serviceOrderDTO = ServiceOrderDTOBuilder.builder().build().toServiceOrderDTO();
+        ServiceOrder serviceOrder = serviceOrderMapper.toModel(serviceOrderDTO);
+
+        // when
+        when(serviceOrderRepository.findByClosed(serviceOrderDTO.getClosed()))
+                .thenReturn(Collections.singletonList(serviceOrder));
+
+        // then
+        List<ServiceOrderDTO> foundListOfServiceOrdersDTO = serviceOrderService.listAllWithClosedStatus(serviceOrderDTO.getClosed());
+
+        assertThat(foundListOfServiceOrdersDTO.get(0), is(equalTo(serviceOrderDTO)));
+        assertThat(foundListOfServiceOrdersDTO, is(not(empty())));
+    }
+
+    @Test
+    void whenListByClosedStatusIsCalledThenAEmptyListShouldBeReturned() {
+        // given
+        Boolean testClosed = true;
+        // when
+        when(serviceOrderRepository.findByClosed(testClosed)).thenReturn(Collections.emptyList());
+
+        // then
+        List<ServiceOrderDTO> foundListOfServiceOrdersDTO = serviceOrderService.listAllWithClosedStatus(testClosed);
+
+        assertThat(foundListOfServiceOrdersDTO, is(empty()));
+    }
+
+    @Test
+    void whenListByCustomerIsCalledThenAListOfServiceOrderShouldBeReturned() {
+        // given
+        ServiceOrderDTO serviceOrderDTO = ServiceOrderDTOBuilder.builder().build().toServiceOrderDTO();
+        ServiceOrder serviceOrder = serviceOrderMapper.toModel(serviceOrderDTO);
+
+        // when
+        when(serviceOrderRepository.findByCustomer(serviceOrder.getCustomer()))
+                .thenReturn(Collections.singletonList(serviceOrder));
+
+        // then
+        List<ServiceOrderDTO> foundListOfServiceOrdersDTO = serviceOrderService.listAllOfCustomer(serviceOrder.getCustomer());
+
+        assertThat(foundListOfServiceOrdersDTO.get(0), is(equalTo(serviceOrderDTO)));
+        assertThat(foundListOfServiceOrdersDTO, is(not(empty())));
+    }
+
+    @Test
+    void whenListByCustomerIsCalledThenAEmptyListShouldBeReturned() {
+        // given
+        Customer testCustomer = CustomerMapper.INSTANCE.toModel(
+                CustomerDTOBuilder.builder().build().toCustomerDTO()
+        );
+        // when
+        when(serviceOrderRepository.findByCustomer(testCustomer)).thenReturn(Collections.emptyList());
+
+        // then
+        List<ServiceOrderDTO> foundListOfServiceOrdersDTO = serviceOrderService.listAllOfCustomer(testCustomer);
+
+        assertThat(foundListOfServiceOrdersDTO, is(empty()));
+    }
+
+    @Test
+    void whenListByServiceTypeIsCalledThenAListOfServiceOrderShouldBeReturned() {
+        // given
+        ServiceOrderDTO serviceOrderDTO = ServiceOrderDTOBuilder.builder().build().toServiceOrderDTO();
+        ServiceOrder serviceOrder = serviceOrderMapper.toModel(serviceOrderDTO);
+
+        // when
+        when(serviceOrderRepository.findByServiceType(serviceOrder.getType()))
+                .thenReturn(Collections.singletonList(serviceOrder));
+
+        // then
+        List<ServiceOrderDTO> foundListOfServiceOrdersDTO = serviceOrderService.listAllWithServiceType(serviceOrder.getType());
+
+        assertThat(foundListOfServiceOrdersDTO.get(0), is(equalTo(serviceOrderDTO)));
+        assertThat(foundListOfServiceOrdersDTO, is(not(empty())));
+    }
+
+    @Test
+    void whenListByServiceTypeIsCalledThenAEmptyListShouldBeReturned() {
+        // given
+        ServiceType testType = ServiceType.INSTALLATION;
+
+        // when
+        when(serviceOrderRepository.findByServiceType(testType)).thenReturn(Collections.emptyList());
+
+        // then
+        List<ServiceOrderDTO> foundListOfServiceOrdersDTO = serviceOrderService.listAllWithServiceType(testType);
+
+        assertThat(foundListOfServiceOrdersDTO, is(empty()));
+    }
+
+    @Test
+    void whenDeleteIsCalledWithAValidServiceOrderIdThenACustomerShouldBeDeleted() throws ServiceOrderNotFoundException {
+        // given
+        ServiceOrderDTO serviceOrderDTO = ServiceOrderDTOBuilder.builder().build().toServiceOrderDTO();
+        ServiceOrder serviceOrder = serviceOrderMapper.toModel(serviceOrderDTO);
+
+        // when
+        when(serviceOrderRepository.findById(serviceOrderDTO.getId())).thenReturn(Optional.of(serviceOrder));
+        doNothing().when(serviceOrderRepository).deleteById(serviceOrderDTO.getId());
+
+        // then
+        serviceOrderService.deleteById(serviceOrderDTO.getId());
+
+        verify(serviceOrderRepository, times(1)).deleteById(serviceOrderDTO.getId());
+    }
+
+    @Test
+    void whenDeleteIsCalledWithAnInvalidServiceOrderIdThenAnExceptionShouldBeThrown() {
+        // given
+        ServiceOrderDTO serviceOrderDTO = ServiceOrderDTOBuilder.builder().build().toServiceOrderDTO();
+
+
+        // when
+        when(serviceOrderRepository.findById(serviceOrderDTO.getId())).thenReturn(Optional.empty());
+
+
+        // then
+        assertThrows(ServiceOrderNotFoundException.class, () -> serviceOrderService.deleteById(serviceOrderDTO.getId()));
+    }
+    @Test
+    void whenUpdateIsCalledWithAValidServiceOrderThenTheServiceOrderShouldBeUpdated() throws ServiceOrderNotFoundException {
+        // given
+        ServiceOrderDTO foundedServiceOrderDTO = ServiceOrderDTOBuilder.builder().build().toServiceOrderDTO();
+        ServiceOrderDTO serviceOrderToUpdateDTO = ServiceOrderDTOBuilder.builder().build().toServiceOrderDTO();
+        serviceOrderToUpdateDTO.setClosed(!foundedServiceOrderDTO.getClosed());
+        serviceOrderToUpdateDTO.setType(ServiceType.REMOVAL);
+
+        ServiceOrder foundedServiceOrder = serviceOrderMapper.toModel(foundedServiceOrderDTO);
+        ServiceOrder serviceOrderToUpdate = serviceOrderMapper.toModel(serviceOrderToUpdateDTO);
+
+        // when
+        when(serviceOrderRepository.findById(foundedServiceOrderDTO.getId())).thenReturn(Optional.of(foundedServiceOrder));
+        when(serviceOrderRepository.save(serviceOrderToUpdate)).thenReturn(serviceOrderToUpdate);
+
+        // then
+        ServiceOrderDTO updatedServiceOrderDTO = serviceOrderService.update(serviceOrderToUpdateDTO);
+
+        assertThat(updatedServiceOrderDTO, equalTo(serviceOrderToUpdateDTO));
+    }
+
+    @Test
+    void whenUpdateIsCalledWithAnInvalidServiceOrderIdThenAnExceptionShouldBeThrown() {
+        // given
+        ServiceOrderDTO serviceOrderDTO = ServiceOrderDTOBuilder.builder().build().toServiceOrderDTO();
+        ServiceOrder serviceOrder = serviceOrderMapper.toModel(serviceOrderDTO);
+
+        // when
+        when(serviceOrderRepository.findById(serviceOrderDTO.getId())).thenReturn(Optional.empty());
+
+        // then
+        assertThrows(ServiceOrderNotFoundException.class, () -> serviceOrderService.update(serviceOrderDTO));
+        verify(serviceOrderRepository, times(0)).save(serviceOrder);
+    }
+
 }
