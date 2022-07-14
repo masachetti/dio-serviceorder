e6f6d0b6dc4aff0e29b8341cc10deacf2fe69caa
masachetti <50852444+masachetti@users.noreply.github.com>
2022-07-14 15:59:19 -0400

    Style - Remove test code from main

--- src/main/java/dio/serviceorder/ServiceorderApplication.java
+++ src/main/java/dio/serviceorder/ServiceorderApplication.java
@@ -20,46 +20,7 @@ import org.springframework.stereotype.Service;
 public class ServiceorderApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ServiceorderApplication.class, args);
+		SpringApplication.	run(ServiceorderApplication.class, args);
 	}
 
-}
-
-@Service
-class RunnerToTest implements CommandLineRunner {
-
-	@Override
-	public void run(String... args) throws Exception {
-		System.out.println("Hello spring");
-		CustomerDTO customerDTO = new CustomerDTO(1L, "Jao");
-		System.out.println("Customer DTO: " + customerDTO);
-		CustomerMapper customerMapper = CustomerMapper.INSTANCE;
-		Customer customer = customerMapper.toModel(customerDTO);
-		System.out.println("Converted Customer: " + customer);
-
-		ServiceOrderDTO serviceOrderDTO = new ServiceOrderDTO(1L, customerDTO, true, ServiceType.INSTALLATION);
-		System.out.println("Service DTO :" + serviceOrderDTO);
-		ServiceOrderMapper serviceOrderMapper = ServiceOrderMapper.INSTANCE;
-		ServiceOrder serviceOrder = serviceOrderMapper.toModel(serviceOrderDTO);
-		System.out.println("Converted Service: " + serviceOrder);
-
-		System.out.println("Json Customer DTO: " + JsonConvertionUtils.asJsonString(customerDTO));
-		System.out.println("Json Service DTO: " + JsonConvertionUtils.asJsonString(serviceOrderDTO));
-
-	}
-}
-
-class JsonConvertionUtils {
-	public static String asJsonString(Object bookDTO) {
-		try {
-			ObjectMapper objectMapper = new ObjectMapper();
-			objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-			objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
-			objectMapper.registerModules(new JavaTimeModule());
-
-			return objectMapper.writeValueAsString(bookDTO);
-		} catch (Exception e) {
-			throw new RuntimeException(e);
-		}
-	}
 }
