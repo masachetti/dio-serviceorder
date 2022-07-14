888757924d630e479b968e80a9e9a60885494903
masachetti <50852444+masachetti@users.noreply.github.com>
2022-07-14 10:20:22 -0400

    Feat - Added methods to service order repository
    Added methods to:
    - Find by closed status
    - Find by customer id
    - Find by service type

--- src/main/java/dio/serviceorder/repository/ServiceOrderRepository.java
+++ src/main/java/dio/serviceorder/repository/ServiceOrderRepository.java
@@ -1,8 +1,15 @@
 package dio.serviceorder.repository;
 
+import dio.serviceorder.enums.ServiceType;
+import dio.serviceorder.model.Customer;
 import dio.serviceorder.model.ServiceOrder;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface ServiceOrderRepository extends JpaRepository<ServiceOrder, Long> {
 
+    List<ServiceOrder> findByClosed(Boolean closed);
+    List<ServiceOrder> findByCustomer(Customer customer);
+    List<ServiceOrder> findByServiceType(ServiceType type);
 }
