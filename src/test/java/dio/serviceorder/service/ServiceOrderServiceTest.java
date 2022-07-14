bc9bed5ceb449b32d5a1ab0fe23d1b482ed35872
masachetti <50852444+masachetti@users.noreply.github.com>
2022-07-14 16:23:33 -0400

    Refactor - Remove temporary comments

--- src/test/java/dio/serviceorder/service/ServiceOrderServiceTest.java
+++ src/test/java/dio/serviceorder/service/ServiceOrderServiceTest.java
@@ -29,19 +29,6 @@ import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class ServiceOrderServiceTest {
-    /*
-    Funcionalidades desejadas:
-        - Adicionar nova ordem de serviço
-        - Buscar uma ordem de serviço
-        - Buscar todas as ordem de serviços
-        - Buscar apenas as ordens abertas
-        - Buscasr apenas as orndens fechadas
-        - Buscar todas as ordens de um cliente
-        - Buscar por tipo de serviço
-        - Alterar ordem de serviço
-        - Deletar ordem de serviço
-    */
-
     @Mock
     private ServiceOrderRepository serviceOrderRepository;
 
